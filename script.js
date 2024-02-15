f4819f7bab8c072c2f750c3ca3d463a580085ace
AnasElmaliki69 <151750800+AnasElmaliki69@users.noreply.github.com>
2024-02-15 13:16:27 +0100

    add file

---
+++ script.js
@@ -0,0 +1,44 @@
+const nav = document.querySelector('.navbar')
+fetch('/navbar.html')
+.then(res=>res.text())
+.then(data=>{
+    nav.innerHTML=data
+})
+
+
+const observer = new IntersectionObserver((entries)=> {
+    entries.forEach((entry)=> {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el)=> observer.observe(el));
+/* test vidéo */ 
+
+var video = document.getElementById("background-video");
+
+var btn = document.getElementById("btnVideo");
+
+ 
+
+function playAndPause () {
+
+if (video.paused) {
+
+video.play();
+
+btn.innerHTML = "Pause II";
+
+} else {
+
+video.pause();
+
+btn.innerHTML = "Play ▶";
+
+}
+
+}
